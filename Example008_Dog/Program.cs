1a1dc223059dc69e41a1214f1c91da9206eadc96
Aleksandr <aldrobakha@gmail.com>
2022-04-11 17:16:38 +0300

    Add Example008_Dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,20 @@
+﻿int distance = 10000, FFS = 1, SFS = 2, dogS = 5, friends = 2, count = 0, time = 0;
+
+while(distance > 10)
+{
+    if (friends == 1) 
+    {
+        time = distance / (SFS + dogS); 
+        friends = 1;
+    }
+    else
+    {
+        time = distance / (FFS + dogS); 
+        friends = 2;
+    }
+    distance = distance - (FFS + SFS) * time;
+    count = count + 1;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");

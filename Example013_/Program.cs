8904f6afec95b31d2e4fe079e110324a7b249a50
Aleksandr <aldrobakha@gmail.com>
2022-04-19 19:59:25 +0300

    add Example 014-016

---
+++ Example013_/Program.cs
@@ -0,0 +1,67 @@
+﻿// Типы методов
+// Вид 1
+
+
+
+
+
+
+
+
+
+// Вид 2
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+    
+}
+Method21("Текст", 4);
+
+
+
+
+
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+
+
+
+
+// Вид 4
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
